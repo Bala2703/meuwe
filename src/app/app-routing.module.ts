2ec2a2e446bc38bf08e8e053018b6fd20fec9d64
Bala2703 <balakumar27032002@gmail.com>
2022-05-18 23:22:51 +0530

    set path to the component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'home' ,component:HeaderComponent},
+  {path : '',redirectTo:"home",pathMatch:"full"},
+  {path : 'login' , component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
